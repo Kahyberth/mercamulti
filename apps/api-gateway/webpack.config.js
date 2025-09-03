9578534f712d5cb7b0154596b00928040703026c
Kahyberth <kahyberth@gmail.com>
2025-09-03 18:26:55 -0500

    Correct the Webpack output path for deployment

--- apps/api-gateway/webpack.config.js
+++ apps/api-gateway/webpack.config.js
@@ -3,7 +3,9 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, 'dist'),
+    // --- LA CORRECCIÓN ESTÁ AQUÍ ---
+    path: join(__dirname, '../../dist/apps/api-gateway'),
+    // -----------------------------
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     }),
